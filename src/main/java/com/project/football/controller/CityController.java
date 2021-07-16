4b617a3e11c169c48ef4b4093f50222b8253f25d
Daniela Cruz <daniela18122012@gmail.com>
2021-07-16 13:59:17 -0600

    stop tracking

--- src/main/java/com/project/football/controller/CityController.java
+++
@@ -1,2 +0,0 @@
-package com.project.football.controller;public class CityController {
-}
--- src/main/java/com/project/football/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.football.repository;
-
-import com.project.football.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long>{
-}
