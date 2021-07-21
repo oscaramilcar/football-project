4122e113d23ee3facd8f316d344272b0f15a532a
Daniela Cruz <daniela18122012@gmail.com>
2021-07-21 11:23:12 -0600

    refactoring

--- src/main/java/com/project/football/services/IStatisticReport.java
+++ src/main/java/com/project/football/services/IStatisticReport.java
@@ -1,11 +1,9 @@
 package com.project.football.services;
 
 import com.project.football.item.TeamClassificationItem;
-import com.project.football.response.TeamResponse;
 
 import java.util.List;
 
 public interface IStatisticReport {
     List<TeamClassificationItem> getGoalsByMatchByLeague(long idLeague);
-    //List<TeamResponse> getMatchStatisticsByTeam(long idLeague);
 }
