a3ea721f2aff3ed2d8a9faa799f97215e09827c0
Michael <michael.bosworth2@gmail.com>
2021-07-21 11:08:38 -0600

    Followed missing classes for JWT

---
+++ src/main/java/com/project/football/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.project.football.services;
+
+
+import com.project.football.model.User;
+import com.project.football.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(email);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
