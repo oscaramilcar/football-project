28b940e24dd704e749b46492211e3f71b4968e79
Daniela Cruz <daniela18122012@gmail.com>
2021-07-21 13:52:21 -0600

    update logic

--- src/main/java/com/project/football/repository/GameMatchRepository.java
+++ src/main/java/com/project/football/repository/GameMatchRepository.java
@@ -21,19 +21,19 @@ public interface GameMatchRepository extends JpaRepository<GameMatch, Long> {
                     "INNER JOIN pm.gameMatch g ON g.localTeam.idTeam = t.idTeam WHERE g.idGameMatch = ?1")
     Optional<Integer> sumGoalsLocalTeam(long idGame);
 
-    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where g.visitorScore=3 AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where (g.visitorScore > g.localScore) AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> visitorWonMatchesByLeague(long idLeague, long idTeam);
-    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where g.localScore=3 AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where (g.localScore > g.visitorScore) AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> localWonMatchesByLeague(long idLeague, long idTeam);
 
-    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where g.visitorScore=0 AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where (g.visitorScore < g.localScore) AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> visitorLostMatchesByLeague(long idLeague, long idTeam);
-    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where g.localScore=0 AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where (g.localScore < g.visitorScore) AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> localLostMatchesByLeague(long idLeague, long idTeam);
 
-    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where g.visitorScore=1 AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.visitorScore) FROM GameMatch g where (g.visitorScore = g.localScore) AND g.visitorTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> visitorTieMatchesByLeague(long idLeague, long idTeam);
-    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where g.localScore=1 AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
+    @Query(value = "SELECT COUNT(g.localScore) FROM GameMatch g where (g.localScore = g.visitorScore) AND g.localTeam.idTeam=?2 AND g.league.idLeague=?1")
     Optional<Integer> localTieMatchesByLeague(long idLeague, long idTeam);
 
     @Query(value = "{CALL getGoalsByMatch(:idL)}", nativeQuery = true)
